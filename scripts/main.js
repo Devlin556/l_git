e76e462312704f9d029c112c6bb5e00ebe76a79b
Алексей Малигон < >
2017-02-12 01:48:54 +0300

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+$('.slider').mousedown(function (ev) {
+  $(ev.target).off('dragstart');
+  var shiftX = ev.pageX - parseInt($(ev.target).offset().left)
+  $(document).on('mousemove',function (e) {
+            var newLeft = e.pageX - shiftX - $('.button').offset().left;
+            if(newLeft < 0) newLeft = 0;
+            var rightEdge = $('.button').width() - $(ev.target).width();
+            if(newLeft > rightEdge) newLeft = rightEdge;
+            console.log(newLeft);
+            if(newLeft < 15) {
+              $('.button').removeClass('green');
+              newLeft = 0
+              $('.button').attr('state',0);
+            }
+            if(newLeft >= 15) {
+              $('.button').addClass('green');
+              newLeft = rightEdge;
+              $('.button').attr('state',1);
+            }
+            $(ev.target).css('left' , newLeft+'px');
+    })
+    $(document).mouseup(function (e) {
+        $(document).off('mousemove');
+    })
+})
